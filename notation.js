6542f5520f78b5d1b63120ea9846423e0e25dfb4
Sayem <sayem465508@gmail.com>
2024-12-23 22:10:40 +0600

    dot notation and bracket notation

---
+++ notation.js
@@ -0,0 +1,14 @@
+const person = {
+    name: 'Sayem Bhuiyan',
+    profession: "Web Developer",
+    age: 22,
+    address: 'Narsingdi'
+}
+
+// Dot Notation
+const prof1 = person.profession;
+
+
+// Bracket Notation
+const prof2 = person["profession"];
+console.log(prof1, prof2);
